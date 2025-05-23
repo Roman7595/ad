13b20026822047a05fb93289cbc94bb84f2947b7
Roman7595 <126889828+Roman7595@users.noreply.github.com>
2025-05-23 06:04:58 +0300

    Update sh1.sh

--- sh1.sh
+++ sh1.sh
@@ -1,19 +1,19 @@
 sudo apt update
-sudo apt install -y openjdk-17-jdk maven git nginx
-java -version
-mvn --version
-mkdir -p /home/roman/app2
-sudo chown -R roman:roman /var/www/app2
-cd /home/roman/app2
-git clone https://github.com/sotovdenis/adm.git
-cd adm
+sudo apt install -y openjdk-17-jdk maven nginx #git
+#java -version
+#mvn --version
+mkdir -p /home/roman/app
+sudo chown -R roman:roman /var/www/app
+cd /home/roman/app
+#git clone https://github.com/sotovdenis/adm.git
+cd ad
 mvn clean package
 sudo cp target/adminka-0.0.1-SNAPSHOT.jar /var/www/app.jar
 sudo echo -e "[Unit]\nDescription=Spring Boot App Service\n[Service]\nWorkingDirectory=/var/www\nExecStart=/usr/bin/java -jar app.jar\nRestart=always\nRestartSec=5\nSyslogIdentifier=springapp\nUser=roman\nEnvironment=SPRING_PROFILES_ACTIVE=prod\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/springapp.service
 sudo systemctl daemon-reload
 sudo systemctl enable springapp
 sudo systemctl start springapp
-sudo systemctl status springapp
+#sudo systemctl status springapp
 sudo chmod 777 /var/www/app.jar
 ls -l /var/www/app.jar
 sudo echo -e "server { \n\tlisten 80;\n\tserver_name 192.168.56.101;" > /etc/nginx/sites-available/springapp
